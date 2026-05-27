a77fcd4f1caf3bcfb56abde2be51db2091afe20e
saiyedin786 <saiyedin786@gmail.com>
2026-05-27 17:51:55 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/ferrous")
+def ferrous_scrap():
+    return render_template("ferrous.html")
+
+@app.route("/nonferrous")
+def nonferrous_scrap():
+    return render_template("nonferrous.html")
+
+@app.route("/our-achievements")
+def achievements():
+    return render_template("achievements.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
